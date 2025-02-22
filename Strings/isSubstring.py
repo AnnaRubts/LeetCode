2aa38a0d2640bead9b695ffdf86b454edd3f2f4c
AnnaRubts <annaro777@gmail.com>
2025-02-22 18:50:16 +0200

    string ex

--- Strings/isSubstring.py
+++ Strings/isSubstring.py
@@ -6,15 +6,21 @@ def isSubstring(txt, pat):
     pat_ind = 0
     txt_ind = 0
     while(txt_ind < len(txt)):
-        while(txt[txt_ind] == pat[pat_ind] and txt_ind < len(txt) and pat_ind < len(pat)):
+        while(txt_ind < len(txt) and pat_ind < len(pat) and txt[txt_ind] == pat[pat_ind]):
             pat_ind+=1
             txt_ind+=1
             if pat_ind == len(pat):
-                return True
+                return True, txt_ind-pat_ind
         pat_ind = 0
         if txt[txt_ind] != pat[pat_ind]:
             txt_ind+=1
-    return False
+    return False, -1
+
+# Follow-up: if a circle is allowed
+# TODO: implement it. idea: concetanate the txt with itself till it is bigger than substring, and check
+def isSubstringCircular(txt, pat):
+    pass
+
 
 if __name__ == "__main__":
     print(isSubstring("Hello", "lo"))
